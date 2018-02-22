c3e82849ec5a20621c29ba9a7622fa29c32a2699
Edward Alexis Ortiz Agudelo - Ceiba Software <edward.ortiz@ceiba.com.co>
2018-02-22 15:38:39 -0500

    Se agrega anotacion Transactional a todas las interfaces de servicios

--- src/main/java/com/ceiba/parking/service/AdminService.java
+++ src/main/java/com/ceiba/parking/service/AdminService.java
@@ -2,8 +2,11 @@ package com.ceiba.parking.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ceiba.parking.model.Admin;
 
+@Transactional
 public interface AdminService {
 
 	List<Admin> findAll();
--- src/main/java/com/ceiba/parking/service/VehicleTypeService.java
+++ src/main/java/com/ceiba/parking/service/VehicleTypeService.java
@@ -1,5 +1,8 @@
 package com.ceiba.parking.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface VehicleTypeService {
 
 }
